6eb627fe9cdc88d2bd158df1d4a9a651b6b47ee2
Tewan <s6652410012@sau.ac.th>
2025-10-09 19:16:20 +0700

    m8.1

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,21 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'geatcbzgqsovildlkagm.supabase.co',
-        port: '',
-        pathname: '/**',
-      }
-    ]
-      
-  },
+  remotePatterns: [
+    {
+      protocol: 'https',
+      hostname: 'geatcbzgqsovildlkagm.supabase.co', // ของ Supabase (อันเดิม)
+      port: '',
+      pathname: '/**',
+    },
+    {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com', // ++ เพิ่มอันนี้สำหรับ Google ++
+      port: '',
+      pathname: '/**',
+    }
+  ]
+},
 };
 
 export default nextConfig;
